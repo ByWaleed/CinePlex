c55119f37685cada8b89d34ca8e7b93fa4d47f43
ByWaleed <waleedi825@gmail.com>
2019-02-27 20:42:07 +0000

    Basic boxed layout created
    https://i.imgur.com/NdLdcpg.png

---
+++ src/main/Main.java
@@ -0,0 +1,37 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("views/allMovies.fxml"));
+
+        /* Title & Icon */
+        primaryStage.setTitle("All Movies");
+        primaryStage.getIcons().add(new Image("main/resources/images/icon.png"));
+
+        /* AllMovies Window Properties */
+        Scene allMovies = new Scene(root, 1200, 600);
+
+
+        primaryStage.setScene(allMovies);
+        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
